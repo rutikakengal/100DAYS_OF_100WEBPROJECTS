6981cec10da8503d2010e6e9e99e8155cffe8aca
Lahari <lahari@Lahari.local>
2025-07-29 19:48:58 +0530

    ✅ Resolved merge conflict in index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const projects = [
   "Email Validator",
   "Snake And Ladder Game",
   "Gradient Background Generator",
-  "Space Jumper Game"
+  "Space Jumper Game",
   // Add more project names as needed
 ];
 
