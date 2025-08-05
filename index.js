60570d2dd07f56366de2606d380237d1c4b084ad
Prakashray-dev <prakashrayias@gmail.com>
2025-08-06 00:02:17 +0530

    feat: Add Day 50 - Sudoku Solver project

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ const projects = [
   "Random Password Generator",
   "Doodle Jump Game",
   "BrainBuzz Quizz Website",
-  "Code Editor"
+  "Code Editor",
+  "SUDOKU"
   // Add more project names as needed
 
 ];
